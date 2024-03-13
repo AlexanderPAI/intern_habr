496b3cb3cc80e69f886cf60ead1f2bd6c68993ce
AlexanderPAI <lawlikeart@yandex.ru>
2024-03-13 20:14:35 +0300

    Linters

--- task1_func.py
+++ task1_func.py
@@ -10,19 +10,14 @@ PROBABILITY_HOME_SCORE = 0.45
 
 OFFSET_MAX_STEP = 3
 
-INITIAL_STAMP = {
-    "offset": 0,
-    "score": {
-        "home": 0,
-        "away": 0
-    }
-}
+INITIAL_STAMP = {"offset": 0, "score": {"home": 0, "away": 0}}
 
 
 def generate_stamp(previous_value):
     score_changed = random.random() > 1 - PROBABILITY_SCORE_CHANGED
-    home_score_change = 1 if score_changed and random.random() > 1 - \
-        PROBABILITY_HOME_SCORE else 0
+    home_score_change = (
+        1 if score_changed and random.random() > 1 - PROBABILITY_HOME_SCORE else 0
+    )
     away_score_change = 1 if score_changed and not home_score_change else 0
     offset_change = math.floor(random.random() * OFFSET_MAX_STEP) + 1
 
@@ -30,13 +25,15 @@ def generate_stamp(previous_value):
         "offset": previous_value["offset"] + offset_change,
         "score": {
             "home": previous_value["score"]["home"] + home_score_change,
-            "away": previous_value["score"]["away"] + away_score_change
-        }
+            "away": previous_value["score"]["away"] + away_score_change,
+        },
     }
 
 
 def generate_game():
-    stamps = [INITIAL_STAMP, ]
+    stamps = [
+        INITIAL_STAMP,
+    ]
     current_stamp = INITIAL_STAMP
     for _ in range(TIMESTAMPS_COUNT):
         current_stamp = generate_stamp(current_stamp)
@@ -55,7 +52,6 @@ pprint(game_stamps)
 # отсортированном по значению offset.
 # Лучше всего, на мой взгляд сделать с использованием бинарного поиска.
 # Асимптотическая сложность: логарифмическая  - O(log(n)).
-
 # Бинарный поиск можно реализовать, через цикл или рекурсию.
 
 
@@ -70,15 +66,15 @@ def get_score(game_stamps, offset):
     while (first_index <= last_index) and stamp == None:
         iter_count += 1
         mid = (first_index + last_index) // 2
-        if game_stamps[mid]['offset'] == offset:
+        if game_stamps[mid]["offset"] == offset:
             stamp = game_stamps[mid]
         else:
-            if offset < game_stamps[mid]['offset']:
+            if offset < game_stamps[mid]["offset"]:
                 last_index = mid - 1
             else:
                 first_index = mid + 1
     if stamp:
-        home, away = dict(**stamp['score']).values()
+        home, away = dict(**stamp["score"]).values()
         return home, away
     return stamp
 
@@ -87,26 +83,22 @@ def get_score_recursion(game_stamps, offset):
     """
     Бинарный поиск через рекурсию.
     """
+
     def binary_search(lst, offset, first_index=0, last_index=None):
         if last_index is None:
             last_index = len(lst) - 1
         if first_index > last_index:
             return None
         mid = (first_index + last_index) // 2
-        if lst[mid]['offset'] == offset:
+        if lst[mid]["offset"] == offset:
             return lst[mid]
-        if offset < lst[mid]['offset']:
+        if offset < lst[mid]["offset"]:
             return binary_search(lst, offset, first_index, mid - 1)
-        if offset > lst[mid]['offset']:
+        if offset > lst[mid]["offset"]:
             return binary_search(lst, offset, mid + 1, last_index)
+
     stamp = binary_search(game_stamps, offset)
     if stamp:
-        home, away = dict(**stamp['score']).values()
+        home, away = dict(**stamp["score"]).values()
         return home, away
     return stamp
-
-
-# stamp_cycle = get_score_cycle(game_stamps, 25001)
-# stamp_iters = get_score_recursion(game_stamps, 25001)
-# pprint(stamp_cycle)
-# pprint(stamp_iters)
--- tests.py
+++ tests.py
@@ -1,11 +1,12 @@
 import random
 import unittest
 
-from task1_func import generate_game, generate_stamp, get_score, get_score_recursion
+from task1_func import generate_game, get_score, get_score_recursion
 
 
 class GetScoreCycleTest(unittest.TestCase):
     """Тестирование get_score"""
+
     @classmethod
     def setUpClass(cls):
         cls.game_stamps = generate_game()
@@ -13,12 +14,14 @@ class GetScoreCycleTest(unittest.TestCase):
     def func_by_existing_offset(self, func):
         """Общая функция тестирования при передаче существующего offset'a."""
         offset = random.choice(self.game_stamps)
-        get_score_cycle_result = func(self.game_stamps, offset['offset'])
-        self.assertEqual(get_score_cycle_result, (offset['score']['home'], offset['score']['away']))
+        get_score_cycle_result = func(self.game_stamps, offset["offset"])
+        self.assertEqual(
+            get_score_cycle_result, (offset["score"]["home"], offset["score"]["away"])
+        )
 
     def func_by_unexisting_offset(self, func):
         """Общая функция тестирования при передаче несуществующего offset'a."""
-        fake_offset = self.game_stamps[-1]['offset'] + 1
+        fake_offset = self.game_stamps[-1]["offset"] + 1
         get_score_cycle_result = func(self.game_stamps, fake_offset)
         self.assertEqual(get_score_cycle_result, None)
 
